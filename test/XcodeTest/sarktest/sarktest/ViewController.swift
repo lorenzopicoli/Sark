cc71803cda2cf45a27eeb0f88970837890acf6e2
Lorenzo Piccoli <lorenzopicoli@me.com>
2016-02-17 08:38:25 -0200

    Created simple xcode project for testing

---
+++ test/XcodeTest/sarktest/sarktest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sarktest
+//
+//  Created by Lorenzo Piccoli on 17/02/16.
+//  Copyright © 2016 Lorenzo Piccoli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
