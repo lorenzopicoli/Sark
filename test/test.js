772bbe8c4b6c1d0717047ada9111c4dc5e7341bc
Lorenzo Piccoli <lorenzopicoli@me.com>
2016-02-24 00:34:28 -0300

    Added authentication tests

--- test/test.js
+++ test/test.js
@@ -72,7 +72,6 @@ describe('Sark Tests', () => {
 		});
 	});
 
-
 	describe('Github integration', (done)=>{
 		it('should clone test repository', (done)=>{
 
@@ -99,6 +98,34 @@ describe('Sark Tests', () => {
 		});
 	});
 
+	describe('Authentication', (done)=>{
+		it('should redirect user to homepage if try to access logged.html without being authenticated', (done)=>{
+		  	chai.request(server.app)
+		  		.get('/logged.html')
+		  		.end(function(err, res){
+		  			var redirect = '/';
+		  			var host = res.req._headers.host;
+		  			var fullHost = 'http://' + host + redirect;
+		  			expect(res.redirects).to.contain(fullHost);
+		  			done();
+		  		});
+		});
+
+		it('should redirect user to logged.html if provided the right password', (done)=>{
+		  	chai.request(server.app)
+		  		.post('/login')
+		  		.send({password: '123'})
+		  		.end(function(err, res){
+		  			var redirect = '/logged.html';
+		  			var host = res.req._headers.host;
+		  			var fullHost = 'http://' + host + redirect;
+		  			expect(res.redirects).to.contain(fullHost);
+		  			done();
+		  		});
+		});
+
+	});
+	
 	describe('Socket.io Communication', (done) =>{
 		it('should recieve build command', (done) =>{
 			var client = io.connect(socketURL, options);
