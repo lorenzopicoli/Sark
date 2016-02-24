7163c29602f0aee3cebd01b27efb4f6b1dfe1731
Lorenzo Piccoli <lorenzopicoli@me.com>
2016-02-23 23:31:05 -0300

    Making index.js more readable

--- src/index.js
+++ src/index.js
@@ -3,26 +3,40 @@ import http from 'http';
 import path from 'path';
 import fs from 'fs';
 import favicon from 'serve-favicon';
+import socket from './server/socket';
 
+/*
+================================
+Server setup
+=================================
+*/
 var app = express();
 var httpServer = http.createServer(app).listen(3000);
 var io = require('socket.io').listen(httpServer);
-var socket = require('./server/socket');
 
+/*
+================================
+Config.json read
+=================================
+*/
 var file = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
 var password = "";
-var currentSess = '';
-var shouldLogin = false;
+var shouldLogin = false; //This is true when the user has typed the right password and setted to false right after the redirect to logged.html
 
+//If we are testing then socket listeners should be added ASAP
 if(process.env.NODE_ENV === 'test'){
 	socket.setupListeners(io);
 }
 
+//Read the password property from the config.json file
+//If there isn't one just terminate the process
 if(file.hasOwnProperty('password')){
 	password = file.password;
 }else{
+	console.log('Invalid config.json');
 	process.exit();
 }
+
 
 var bodyParser = require('body-parser')
 app.use(bodyParser.json() );       
@@ -33,6 +47,7 @@ app.use(bodyParser.urlencoded({
 app.use(favicon(__dirname + '/public/favicon.ico'));
 
 app.post('/login', (req, res)=>{
+	//If the password is right redirect to logged.html
 	if (req.body.password === password){
 		shouldLogin = true;
 		res.redirect('/logged.html');
@@ -41,6 +56,7 @@ app.post('/login', (req, res)=>{
 	}
 });
 
+//Check if the user can redirect in case the requested page was logged.html
 app.use('*', (req, res, next)=>{
 	if(req.originalUrl === '/logged.html' && !shouldLogin){
 		res.redirect('/');
@@ -50,13 +66,17 @@ app.use('*', (req, res, next)=>{
 	}
 })
 
+//If the user is going for logged.html and is here that means that the password is right
+//We setup socket.io listeners
 app.use('/logged.html', (req, res, next) =>{
 	socket.setupListeners(io);
 	next();
 });
 
+//Serve static files from /public
 app.use(express.static(__dirname + '/public'));
 
+//Any other page that falls here doesn't exists
 app.get('*', function(req, res){
   res.status(404).send('This page doesn\'t exists');
 });
